02853d866e44ceab39a3ea14f4291e0e976eb12a
Peter Kalinowski <peterkalinowski@Peters-Mac.lan>
2025-05-14 22:29:56 -0400

    exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameNames = new List<string>
+            {
+                "Minecraft", 
+                "Fortnite", 
+                "League of Legends", 
+                "Call of Duty", 
+                "Overwatch",
+                "Apex Legends",
+                "Valorant",
+                "Counter-Strike",
+                "Dota 2",
+                "World of Warcraft",
+                "Final Fantasy",
+                "The Legend of Zelda",
+                "Super Mario",
+                "The Witcher",
+                "Dark Souls",
+                "Skyrim",
+                "Red Dead Redemption",
+                "Grand Theft Auto",
+                "Assassin's Creed",
+            };
+            
+            gameNames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
